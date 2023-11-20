dde6bab6310a9a92c235c97db2ecc8820735ef52
fadedreams7 <fadedreams7@gmail.com>
2023-11-20 10:24:18 -0500

    generateitemimage+

--- app/src/provider/item/item.service.ts
+++ app/src/provider/item/item.service.ts
@@ -2,6 +2,8 @@
 import { ItemModel, uploadDir } from "@fadedreams7pcplatform/common";
 import { Item } from "./item.model";
 import { CreateItemDto, UpdateItemDto, DeleteItemDto, AddImagesDto, DeleteImagesDto } from '../dtos/item.dto'
+import fs from 'fs'
+import path from 'path'
 
 export class ItemService {
   constructor(public itemModel: ItemModel) { }
@@ -11,6 +13,7 @@ export class ItemService {
   }
 
   async create(createItemDto: CreateItemDto) {
+    const images = this.generateItemImages(createItemDto.files);
     const item = new this.itemModel({
       title: createItemDto.title,
       price: createItemDto.price,
@@ -22,5 +25,23 @@ export class ItemService {
   }
 
 
+  generateBase64Url(contentType: string, buffer: Buffer) {
+    return `data:${contentType};base64,${buffer.toString('base64')}`
+  }
+  generateItemImages(files: CreateItemDto['files']): Array<{ src: string }> {
+    let images: Array<Express.Multer.File>;
+
+    if (typeof files === "object") {
+      images = Object.values(files).flat()
+    } else {
+      images = files ? [...files] : []
+    }
+
+    return images.map((file: Express.Multer.File) => {
+      let srcObj = { src: this.generateBase64Url(file.mimetype, fs.readFileSync(path.join(uploadDir + file.filename))) }
+      fs.unlink(path.join(uploadDir + file.filename), () => { })
+      return srcObj
+    })
+  }
 
 }
