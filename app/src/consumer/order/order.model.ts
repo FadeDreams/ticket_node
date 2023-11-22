4f85b18973228a0572343bdcf6539b709e9b37e6
fadedreams7 <fadedreams7@gmail.com>
2023-11-22 03:03:24 -0500

    order s

---
+++ app/src/consumer/order/order.model.ts
@@ -0,0 +1,16 @@
+
+import mongoose from 'mongoose'
+import { OrderDoc, OrderModel } from '@fadedreams7pcplatform/common'
+
+const schema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  totalAmount: { type: Number, required: true },
+
+  chargeId: { type: String, required: true }
+})
+
+export const Order = mongoose.model<OrderDoc, OrderModel>('Order', schema);
